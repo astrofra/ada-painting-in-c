43363ea72915b0ad93af84f510503fd6cd0730c1
fra <fra@movida-mail.com>
2023-10-23 16:39:48 +0200

    Actual fixed point maths.

--- main.c
+++ main.c
@@ -1,18 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void){
-    FILE *bitmap, *bitmap_out;
-    int type,w,h,d;
+#define FIXED_BITS_WIDTH 4
+#define R_FP_FACTOR ((int)(0.2126f * (1 << FIXED_BITS_WIDTH)))
+#define G_FP_FACTOR ((int)(0.7152f * (1 << FIXED_BITS_WIDTH)))
+#define B_FP_FACTOR ((int)(0.0722f * (1 << FIXED_BITS_WIDTH)))
+
+int main(void) {
+    FILE* bitmap, * bitmap_out;
+    int type, w, h, d;
 
     printf("RGB Color Test\n");
-    bitmap = fopen("ada.pbm", "r");
-    bitmap_out = fopen("out.pbm", "w");
+    bitmap = fopen("ada.pbm", "rb");
+    bitmap_out = fopen("out.pbm", "wb");
 
-    if (bitmap){
-        if (bitmap_out){
-            fscanf(bitmap, "P%d%d%d%d", &type, &w, &h, &d);
-            printf("type = %d, width = %d, height = %d, palette size = %d\n", type, w, h, d);
+    if (bitmap) {
+        if (bitmap_out) {
+            int ret;
+            ret = fscanf(bitmap, "P%d%d%d%d\n\r", &type, &w, &h, &d);
+            printf("type = %d, width = %d, height = %d, palette size = %d, returned %d\n", type, w, h, d, ret);
 
             fprintf(bitmap_out, "P%d %d %d %d ", type, w, h, d);
 
@@ -20,17 +26,19 @@ int main(void){
             // and convert it
             {
                 int pixel_index;
-                unsigned char r, g, b, l;
-                for(pixel_index = 0; pixel_index < w * h; pixel_index++){
+                unsigned char r, g, b;
+                int l;
+                for (pixel_index = 0; pixel_index < w * h; pixel_index++) {
                     fread(&r, sizeof(unsigned char), 1, bitmap);
                     fread(&g, sizeof(unsigned char), 1, bitmap);
                     fread(&b, sizeof(unsigned char), 1, bitmap);
 
-                    // Do your conversion process here
+                    l = (unsigned char)((r * R_FP_FACTOR + g * G_FP_FACTOR + b * B_FP_FACTOR) >> FIXED_BITS_WIDTH);
 
-                    fwrite(&r, 1, 1, bitmap_out);
-                    fwrite(&r, 1, 1, bitmap_out);
-                    fwrite(&r, 1, 1, bitmap_out);
+                    // Do your conversion process here
+                    fwrite(&l, sizeof(unsigned char), 1, bitmap_out);
+                    fwrite(&l, sizeof(unsigned char), 1, bitmap_out);
+                    fwrite(&l, sizeof(unsigned char), 1, bitmap_out);
                 }
             }
             fclose(bitmap_out);
